36774173c4cf041811b9795df8df3daceeb21cc0
RealAntEngineer <lafourmiedu35@gmail.com>
2025-02-05 22:13:43 +0100

    worley noise once again

--- src/main/java/com/rae/creatingspace/content/worldgen/WorleyNoise.java
+++ src/main/java/com/rae/creatingspace/content/worldgen/WorleyNoise.java
@@ -35,17 +35,13 @@ public class WorleyNoise {
         return ((34.0d * x + 1.0d) * x) % 289.0d;
     }
 
-    public static Vec3 fract(Vec3 v) {
-        return new Vec3(v.x() - Math.floor(v.x()),
-                v.y() - Math.floor(v.y()),
-                v.z() - Math.floor(v.z()));
+    public static double fract(double x) {
+        return x - Math.floor(x);
     }
 
-    public static double cellular3x3x3(Vec3 P) {
-        Vec3 Pi = new Vec3(Math.floor(P.x()),
-                Math.floor(P.y()),
-                Math.floor(P.z())); // Integer part
-        Vec3 Pf = fract(P); // Fractional part
+    public static double cellular3x3x3(double px, double py, double pz) {
+        double Pix = Math.floor(px), Piy = Math.floor(py), Piz = Math.floor(pz); // Integer part
+        double Pfx = fract(px),Pfy = fract(py),Pfz = fract(pz); // Fractional part
 
         double minDist = Float.MAX_VALUE;
 
@@ -54,7 +50,7 @@ public class WorleyNoise {
                 for (int zi = -1; zi <= 1; zi++) {
                     // Compute cell coordinates
                     //Vec3 cell = Pi.add(new Vec3(xi, yi, zi));
-                    double permuted = permute(permute(permute(Pi.x + xi)+Pi.y+yi)+Pi.z+zi);
+                    double permuted = permute(permute(permute(Pix + xi)+Piy+yi)+Piz+zi);
                     // Pseudo-random offset inside cell
                     double jitterX = ((permuted*K-Math.floor(permuted*K))-Ko) * jitter;
                     double jitterY = ((Math.floor(permuted*K)%7.0) * K-Ko) * jitter;
@@ -62,21 +58,19 @@ public class WorleyNoise {
 
 
                     // Compute squared distance
-                    double dist = Math.sqrt((jitterX+xi-Pf.x)*(jitterX+xi-Pf.x)+(jitterY+yi-Pf.y)*(jitterY+yi-Pf.y)+(jitterZ+zi-Pf.z)*(jitterZ+zi-Pf.z));
+                    double dist = Math.sqrt((jitterX+xi-Pfx)*(jitterX+xi-Pfx)+(jitterY+yi-Pfy)*(jitterY+yi-Pfy)+(jitterZ+zi-Pfz)*(jitterZ+zi-Pfz));
 
                     // Track minimum distance
                     minDist = Math.min(minDist, dist);
                 }
             }
         }
-        return (float) Math.sqrt(minDist); // Return the actual distance
+        return Math.sqrt(minDist); // Return the actual distance
     }
 
     public double getValue(double x, double y, double z) {
-        Vec3 P = new Vec3((x) / XZSize, (y) / YSize, (z) / XZSize);
-        double F = cellular3x3x3(P)*scaleFactor;
+        double F = cellular3x3x3((x) / XZSize,(y) / YSize,(z) / XZSize)*scaleFactor;
         return 1- (F * 2);  // Mapping to range [-1, 1]
     }
-
 }
 
